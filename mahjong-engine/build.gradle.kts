546343e9f7a1f16b36456eb8c5558a2159838ec7
Lilly Bergen <97750344+lhbergen@users.noreply.github.com>
2026-09-11 09:14:45 -0600

    added dependencies to build.gradle.kts

--- mahjong-engine/build.gradle.kts
+++ mahjong-engine/build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.junit.platform:junit-platform-suite-api")
+    testRuntimeOnly("org.junit.platform:junit-platform-suite-engine")
+
+    testImplementation("io.cucumber:cucumber-picocontainer:7.21.1")
+    testImplementation("io.cucumber:cucumber-java:7.21.1")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:7.21.1")
 }
 
 tasks.test {
--- mahjong-engine/src/test/java/cucumber/RunCucumberTest.java
+++ mahjong-engine/src/test/java/cucumber/RunCucumberTest.java
@@ -1,5 +1,12 @@
 package cucumber;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
